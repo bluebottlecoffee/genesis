c7eed25fa1c9e1fc79b6127f1fa14e665a6f54f0
Kyle Wiest <kyle.wiest@gmail.com>
2020-03-12 16:09:43 -0700

    Track config environment

--- lib/genesis/persistence.rb
+++ lib/genesis/persistence.rb
@@ -35,11 +35,12 @@ module Genesis
     class Configuration
       InvalidEnvironmentError = Class.new(RuntimeError)
 
-      attr_accessor :adapter, :database, :host, :port, :username
+      attr_accessor :environment, :adapter, :database, :host, :port, :username
 
       def initialize(env)
         raise InvalidEnvironmentError, "invalid environment: #{env}" unless ENVIRONMENTS.include?(env)
 
+        @environment = env
         @adapter = 'postgres'
         load_config(env)
       end
